0c924cd71dbd2ca846cc9600393c111c4d632de4
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-11-10 14:47:26 +0100

    interactivity

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
+	"strings"
 )
 
 var rootCmd = &cobra.Command{
@@ -12,13 +14,43 @@ var rootCmd = &cobra.Command{
 	Long: `A test usage with long
 			description`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Command is being executed")
+		RunGameLoop()
 	},
 }
 
 func main() {
+	rootCmd.AddCommand()
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+var action1Cmd = &cobra.Command{
+	Use:   "action 1",
+	Short: "action one details",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Action 1 command run")
+	},
+}
+
+func RunGameLoop() {
+	fmt.Println("Welcome to the interactive cli")
+
+	for {
+		fmt.Println("\nAvailable actions: action1, action2, exit")
+		fmt.Println("Choose an action: ")
+
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "action1":
+			action1Cmd.Run(nil, nil)
+		default:
+			fmt.Println("Invalid action, please try again.")
+		}
 	}
 }
